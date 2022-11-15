afb11f1b6ecb47c1bd3d429a04cb11e7be90a05c
Alex Grintsvayg <git@grin.io>
2022-11-15 11:15:22 -0500

    bit shifting

--- go/evm.go
+++ go/evm.go
@@ -41,27 +41,41 @@ type TestCase struct {
 }
 
 const (
-	opStop       = 0x00
-	opAdd        = 0x01
-	opMul        = 0x02
-	opSub        = 0x03
-	opDiv        = 0x04
-	opSDiv       = 0x05
-	opMod        = 0x06
-	opSMod       = 0x07
-	opAddMod     = 0x08
-	opMulMod     = 0x09
-	opExp        = 0x0a
+	opStop = 0x00
+
+	opAdd    = 0x01
+	opMul    = 0x02
+	opSub    = 0x03
+	opDiv    = 0x04
+	opSDiv   = 0x05
+	opMod    = 0x06
+	opSMod   = 0x07
+	opAddMod = 0x08
+	opMulMod = 0x09
+	opExp    = 0x0a
+
 	opSignExtend = 0x0b
-	opLT         = 0x10
-	opGT         = 0x11
-	opSLT        = 0x12
-	opSGT        = 0x13
-	opEQ         = 0x14
-	opIsZero     = 0x15
-	opPop        = 0x50
-	opPush1      = 0x60
-	opPush32     = 0x7f
+
+	opLT     = 0x10
+	opGT     = 0x11
+	opSLT    = 0x12
+	opSGT    = 0x13
+	opEQ     = 0x14
+	opIsZero = 0x15
+
+	opAnd = 0x16
+	opOr  = 0x17
+	opXor = 0x18
+	opNot = 0x19
+
+	opByte = 0x1a
+	opShl  = 0x1b
+	opShr  = 0x1c
+	opSar  = 0x1d
+
+	opPop    = 0x50
+	opPush1  = 0x60
+	opPush32 = 0x7f
 )
 
 func evm(code []byte) (success bool, stack []uint256.Int) {
@@ -138,51 +152,59 @@ func evm(code []byte) (success bool, stack []uint256.Int) {
 		case opLT:
 			var x, y uint256.Int
 			stack, x, y = pop2(stack)
-			if x.Lt(&y) {
-				stack = push(stack, uint256.NewInt(1).Bytes()...)
-			} else {
-				stack = push(stack, uint256.NewInt(0).Bytes()...)
-			}
+			stack = pushBool(stack, x.Lt(&y))
 		case opGT:
 			var x, y uint256.Int
 			stack, x, y = pop2(stack)
-			if x.Gt(&y) {
-				stack = push(stack, uint256.NewInt(1).Bytes()...)
-			} else {
-				stack = push(stack, uint256.NewInt(0).Bytes()...)
-			}
+			stack = pushBool(stack, x.Gt(&y))
 		case opSLT:
 			var x, y uint256.Int
 			stack, x, y = pop2(stack)
-			if x.Slt(&y) {
-				stack = push(stack, uint256.NewInt(1).Bytes()...)
-			} else {
-				stack = push(stack, uint256.NewInt(0).Bytes()...)
-			}
+			stack = pushBool(stack, x.Slt(&y))
 		case opSGT:
 			var x, y uint256.Int
 			stack, x, y = pop2(stack)
-			if x.Sgt(&y) {
-				stack = push(stack, uint256.NewInt(1).Bytes()...)
-			} else {
-				stack = push(stack, uint256.NewInt(0).Bytes()...)
-			}
+			stack = pushBool(stack, x.Sgt(&y))
 		case opEQ:
 			var x, y uint256.Int
 			stack, x, y = pop2(stack)
-			if x.Eq(&y) {
-				stack = push(stack, uint256.NewInt(1).Bytes()...)
-			} else {
-				stack = push(stack, uint256.NewInt(0).Bytes()...)
-			}
+			stack = pushBool(stack, x.Eq(&y))
 		case opIsZero:
 			var x uint256.Int
 			stack, x = pop1(stack)
-			if x.IsZero() {
-				stack = push(stack, uint256.NewInt(1).Bytes()...)
-			} else {
-				stack = push(stack, uint256.NewInt(0).Bytes()...)
-			}
+			stack = pushBool(stack, x.IsZero())
+		case opAnd:
+			var x, y uint256.Int
+			stack, x, y = pop2(stack)
+			stack = push(stack, uint256.NewInt(0).And(&x, &y).Bytes()...)
+		case opOr:
+			var x, y uint256.Int
+			stack, x, y = pop2(stack)
+			stack = push(stack, uint256.NewInt(0).Or(&x, &y).Bytes()...)
+		case opXor:
+			var x, y uint256.Int
+			stack, x, y = pop2(stack)
+			stack = push(stack, uint256.NewInt(0).Xor(&x, &y).Bytes()...)
+		case opNot:
+			var x uint256.Int
+			stack, x = pop1(stack)
+			stack = push(stack, uint256.NewInt(0).Not(&x).Bytes()...)
+		case opByte:
+			var x, y uint256.Int
+			stack, x, y = pop2(stack)
+			stack = push(stack, y.Byte(&x).Bytes()...)
+		case opShl:
+			var x, y uint256.Int
+			stack, x, y = pop2(stack)
+			stack = push(stack, uint256.NewInt(0).Lsh(&y, uint(x.Uint64())).Bytes()...)
+		case opShr:
+			var x, y uint256.Int
+			stack, x, y = pop2(stack)
+			stack = push(stack, uint256.NewInt(0).Rsh(&y, uint(x.Uint64())).Bytes()...)
+		case opSar:
+			var x, y uint256.Int
+			stack, x, y = pop2(stack)
+			stack = push(stack, uint256.NewInt(0).SRsh(&y, uint(x.Uint64())).Bytes()...)
 		}
 	}
 
@@ -195,6 +217,15 @@ func push(stack []uint256.Int, data ...byte) []uint256.Int {
 	}
 	i := uint256.NewInt(0).SetBytes(data)
 	return append([]uint256.Int{*i}, stack...)
+}
+
+func pushBool(stack []uint256.Int, b bool) []uint256.Int {
+	if b {
+		stack = push(stack, uint256.NewInt(1).Bytes()...)
+	} else {
+		stack = push(stack, uint256.NewInt(0).Bytes()...)
+	}
+	return stack
 }
 
 func pop(stack []uint256.Int, n int) ([]uint256.Int, []uint256.Int) {
